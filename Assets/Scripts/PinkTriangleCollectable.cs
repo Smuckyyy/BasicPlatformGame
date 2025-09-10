c8c1c2fe3e6c1a34e17a930032aba35ac178a94d
Smuckyyy <mrollins626@gmail.com>
2025-09-10 16:47:15 -0400

    Added Aspects
    Jumping, Player Flipping, and a Collectable were added

---
+++ Assets/Scripts/PinkTriangleCollectable.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PinkTriangleCollectable : MonoBehaviour
+{
+    string test;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public string getTestString()
+    {
+        test = "Hello from Pink Collectable";
+
+        return test;
+    }
+
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
-using System.Diagnostics;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     private float inputHorizontal;
+    private int maxNumJumps;
+    private int numJumps;
     //Because this is public, we have access to it in the unity editor.
     //Do not put a speed and have it = to anything, we will do that in Unity editor
     public float horizontalMoveSpeed;
+    //We dont look for equality in Unity because of frame rates, always looking for greater than for things like moving.
+    //Ex. (If I get to space 4 -> do this. Instead, If I get anything greater than space 4 -> do this)
+    public float jumpForce;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,15 +19,17 @@ public class PlayerController : MonoBehaviour
         //I can only get this component because the rigidbody2d is attached to the player
         //This script is also attached to the player
         rb = GetComponent<Rigidbody2D>();
+        maxNumJumps = 1;
+        numJumps = maxNumJumps;
 
-        //Debug.Log("Hello from Player Controller!");
+        Debug.Log("Hello from Player Controller!");
     }
 
     // Update is called once per frame
     void Update()
     {
         movePlayerLateral();
-        //jump();
+        jump();
     }
 
     private void movePlayerLateral()
@@ -35,8 +41,52 @@ public class PlayerController : MonoBehaviour
         //1 - right arrow or D pressed
         //2 - left arrow or A pressed
         inputHorizontal = Input.GetAxisRaw("Horizontal");
+        flipPlayerSprite(inputHorizontal);
+        rb.linearVelocity = new UnityEngine.Vector2(horizontalMoveSpeed * inputHorizontal, rb.linearVelocity.y);
 
-        rb.linearVelocity = new Vector2(horizontalMoveSpeed * inputHorizontal, rb.linearVelocity.y);
+    }
 
+    private void flipPlayerSprite(float inputHorizontal)
+    {
+        //This will flip the sprite based on the direction the player is moving
+        //We are using vector3 because unity is "always" in 3D
+        if (inputHorizontal > 0)
+        {
+            transform.eulerAngles = new UnityEngine.Vector3(0, 0, 0);
+        }
+        else if (inputHorizontal < 0)
+        {
+            transform.eulerAngles = new UnityEngine.Vector3(0, 180, 0);
+        }
+    }
+
+    private void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && numJumps <= maxNumJumps)
+        {
+            rb.linearVelocity = new UnityEngine.Vector2(rb.linearVelocity.x, jumpForce);
+            numJumps++;
+        }
+    }
+
+    //Collisions
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Collision will contain information about the object that the player collided with
+        //Debug.Log(collision.gameObject);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            numJumps = maxNumJumps;
+        }
+    }
+
+    //Triggers
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PinkCollectable"))
+        {
+            string fromPinkCollectable = collision.gameObject.GetComponent<PinkTriangleCollectable>().getTestString();
+            Debug.Log(fromPinkCollectable);
+        }
     }
 }
